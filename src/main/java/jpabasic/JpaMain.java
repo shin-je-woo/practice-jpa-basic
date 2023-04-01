acd08f90969c7ba46cb21bddd33deb0db1fdc160
shin.jewoo <shinjw0926@naver.com>
2023-04-01 18:29:51 +0900

    연관관계 매핑 기초 - 양방향 연관관계와 연관관계의 주인

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -26,17 +27,21 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("memberA");
-            member.setTeam(team);
+            member.changeTeam(team);
             em.persist(member);
 
             //영속성 컨텍스트 초기화
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            System.out.println("================");
+            for (Member m : members) {
+                System.out.println("m.getName() = " + m.getName());
+            }
+            System.out.println("================");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabasic/domain/Team.java
+++ src/main/java/jpabasic/domain/Team.java
@@ -3,10 +3,9 @@ package jpabasic.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -18,4 +17,7 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
 }
